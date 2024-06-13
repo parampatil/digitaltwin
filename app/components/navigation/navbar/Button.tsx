999a31d3ddc865c2c6f5a10499b9bd23b4913a1f
parampatil <paramrpatil@gmail.com>
2024-06-12 22:20:16 -0400

    Initial Commit

---
+++ app/components/navigation/navbar/Button.tsx
@@ -0,0 +1,6 @@
+const Button = () => {
+    return (
+      <button className="h-12 rounded-lg bg-white text-black font-bold px-5">Sign In</button>
+    );
+  };
+  export default Button;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Navigation from "./components/navigation";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Navigation />
+        {children}
+      </body>
     </html>
   );
 }
